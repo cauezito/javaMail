7d9ee1e6bc39c575b2c8efb62804ce80687b8abb
Cauê Santos <caah.sfc@live.com>
2020-06-20 16:30:15 -0300

    Criando a estrutura do projeto

---
+++ java-mail/src/main/java/br/com/cauezito/java_mail/java_mail/App.java
@@ -0,0 +1,13 @@
+package br.com.cauezito.java_mail.java_mail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java-mail/src/test/java/br/com/cauezito/java_mail/java_mail/AppTest.java
@@ -0,0 +1,10 @@
+package br.com.cauezito.java_mail.java_mail;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+@Test
+public class AppTest {}
+
+
