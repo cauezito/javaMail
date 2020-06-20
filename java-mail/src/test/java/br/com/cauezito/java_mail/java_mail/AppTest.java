05bb450c0464c3b1291d855868215e68c6225493
Cauê Santos <caah.sfc@live.com>
2020-06-20 16:39:21 -0300

    Configurando as propriedades de conexão

--- java-mail/src/test/java/br/com/cauezito/java_mail/java_mail/AppTest.java
+++ java-mail/src/test/java/br/com/cauezito/java_mail/java_mail/AppTest.java
@@ -1,10 +1,23 @@
 package br.com.cauezito.java_mail.java_mail;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Properties;
 
 import org.junit.Test;
 
-@Test
-public class AppTest {}
+public class AppTest {
+	
+	@Test
+	public void testEmail() {
+		/*configs SMTP*/
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true"); /*Autorização*/
+		properties.put("mail.smtp.starttls", "true"); /*Autenticação*/
+		properties.put("mail.smtp.host", "smtp.gmail.com"); /*Servidor gmail*/
+		properties.put("mail.smtp.port", "465"); /*Porta do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465"); /*Especifica a porta a ser conectada pelo socket*/
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); /*Classe socket de conexão ao smtp*/
+		
+	}
+}
 
 
