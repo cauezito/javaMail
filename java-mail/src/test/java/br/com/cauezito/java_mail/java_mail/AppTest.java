317af74dc2dec5beed18094103c0947303a7c57d
Cauê Santos <caah.sfc@live.com>
2020-06-20 17:52:57 -0300

    Criando a classe SendEmail para reutilizar

--- java-mail/src/test/java/br/com/cauezito/java_mail/java_mail/AppTest.java
+++ java-mail/src/test/java/br/com/cauezito/java_mail/java_mail/AppTest.java
@@ -1,51 +1,17 @@
 package br.com.cauezito.java_mail.java_mail;
 
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.junit.Test;
+
+import br.com.cauezito.java_mail.SendEmail;
 
 public class AppTest {
 	
-	private String user = "";
-	private String password = "";
 	
 	@Test
 	public void testEmail() {
 		try {
-			/*configs SMTP*/
-			Properties properties = new Properties();
-			properties.put("mail.smtp.ssl.trust", "*");
-			properties.put("mail.smtp.auth", "true"); /*Autorização*/
-			properties.put("mail.smtp.starttls", "true"); /*Autenticação*/
-			properties.put("mail.smtp.host", "smtp.gmail.com"); /*Servidor gmail*/
-			properties.put("mail.smtp.port", "465"); /*Porta do servidor*/
-			properties.put("mail.smtp.socketFactory.port", "465"); /*Especifica a porta a ser conectada pelo socket*/
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); /*Classe socket de conexão ao smtp*/
-			
-			Session session = Session.getInstance(properties, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(user, password);
-				}
-			});
-			
-			
-			Address[] toUser = InternetAddress.parse(""); //add e-mails 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(user, "Cauê Santos"));
-			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("E-mail de teste.");
-			message.setContent("Deu tudo certo!", "text/html");
-			Transport.send(message);
+			SendEmail sendEmail = new SendEmail("nokylevs@gmail.com", "Cauê", "Teste!", "Chegou?");
+			sendEmail.send();
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
